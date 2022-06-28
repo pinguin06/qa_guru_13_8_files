53e04a67023751041a35ff22e34986db1d087f33
Olga Romanshchak <pinguin06@rambler.ru>
2022-06-28 15:39:56 +0300

    class separation: zip / unzip

--- src/test/java/guru/qa/HomeWorkUnZIP.java
+++ src/test/java/guru/qa/HomeWorkUnZIP.java
@@ -12,52 +12,20 @@ import java.io.*;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class HomeWork {
+public class HomeWorkUnZIP {
     public static final String FILE = "src\\test\\resources\\output.zip";
-    ClassLoader classLoader = FileParseTest.class.getClassLoader();
+    static ClassLoader classLoader = FileParseTest.class.getClassLoader();
 
 
     @DisplayName("Запись в архив")
     @Test
-    public static void main(String[] args) throws IOException {
-        //где искать файлы, которые нужно поместить в архив
-        String parent = "src/test/resources/filesForZip";
+    public static void main(String[] args) throws IOException, CsvException {
 
-        //создаем архив
-        ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(FILE));
-
-        //сохраняем список файлов
-        File[] files = new File(parent, "").listFiles();
-        for (File f : files) {
-
-            //создаём entry для помещения в архив
-            ZipEntry file = new ZipEntry(f.getName());
-
-            //помещаем в архив
-            zout.putNextEntry(file);
-
-            //читаем байты из файла и помещаем в буфер
-            FileInputStream fis = new FileInputStream(f);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-
-            //записываем байты в архив
-            zout.write(buffer);
-
-            //закрываем поток для entry
-            zout.closeEntry();
-        }
-        //закрываем поток для архива
-        zout.close();
-    }
-
-    public void unZIPCheck() throws IOException, CsvException {
         String parent = "src/test/resources/unZIP";
         InputStream is = classLoader.getResourceAsStream("output.zip");
         ZipInputStream zis = new ZipInputStream(is);
---
+++ src/test/java/guru/qa/HomeWorkZIP.java
@@ -0,0 +1,49 @@
+package guru.qa;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class HomeWorkZIP {
+    public static final String FILE = "src\\test\\resources\\output.zip";
+    ClassLoader classLoader = FileParseTest.class.getClassLoader();
+
+
+    @DisplayName("Запись в архив")
+    @Test
+    public static void main(String[] args) throws IOException {
+        //где искать файлы, которые нужно поместить в архив
+        String parent = "src/test/resources/filesForZip";
+
+        //создаем архив
+        ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(FILE));
+
+        //сохраняем список файлов
+        File[] files = new File(parent, "").listFiles();
+        for (File f : files) {
+
+            //создаём entry для помещения в архив
+            ZipEntry file = new ZipEntry(f.getName());
+
+            //помещаем в архив
+            zout.putNextEntry(file);
+
+            //читаем байты из файла и помещаем в буфер
+            FileInputStream fis = new FileInputStream(f);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+
+            //записываем байты в архив
+            zout.write(buffer);
+
+            //закрываем поток для entry
+            zout.closeEntry();
+        }
+        //закрываем поток для архива
+        zout.close();
+    }
+}
